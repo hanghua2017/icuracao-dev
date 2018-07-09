4eac9e590e21e069e2bfedc06842e793b8249f8d
Sooraj Sathyan <sooraj@terrificminds.com>
2018-07-09 19:46:07 +0530

    Account number validation completed

---
+++ app/code/Dyode/ArInvoice/Helper/Data.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package   Dyode
+ * @author    Sooraj Sathyan
+ */
+
+namespace Dyode\ArInvoice\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    // To check the account number is required format
+    public function validateAccountNumber($accountNumber)
+    {
+        // dummy content
+        $accountNumber = "52041";
+        if (strlen($accountNumber) == 7) {
+            $accountNumberFormatted = substr_replace($accountNumber, "-", 3, 0);
+        }
+        elseif (strlen($accountNumber) < 7) {
+            $accountNumber = str_pad($accountNumber, 7, "0", STR_PAD_LEFT);
+            $accountNumberFormatted = substr_replace($accountNumber, "-", 3, 0);
+        }
+        else {
+            $accountNumberFormatted = $accountNumber;   
+        }
+
+        // Regular expression for Account Numbers
+        $regex = "/^[0-9]{3}-[0-9]{4}$/";
+
+        if (!preg_match($regex, $accountNumberFormatted)) {
+            $soapClient = $this->setSoapClient();
+            $customerStatus = $this->checkCustomerStatus($accountNumberFormatted);
+            #incomplete ...
+        }
+        else {
+            return $accountNumberFormatted;
+        }
+    }
+
+    //Setting up the Soap Client
+    public function setSoapClient()
+    {
+        $wsdlUrl = 'https://exchangeweb.lacuracao.com:2007/ws1/test/ecommerce/Main.asmx?WSDL';
+        $soapClient = new \SoapClient($wsdlUrl,['version' => SOAP_1_2]);
+        $xmlns = 'http://lacuracao.com/WebServices/eCommerce/';
+        $headerbody = array('UserName' => 'mike',
+            'Password' => 'ecom12');
+        //Create Soap Header.
+        $header = new \SOAPHeader($xmlns, 'TAuthHeader', $headerbody);
+        //set the Headers of Soap Client.
+        $soapHeader = $soapClient->__setSoapHeaders($header);
+        return $soapClient;
+    }
+}
--- app/code/Dyode/ArInvoice/Model/OrderCollection.php
+++ app/code/Dyode/ArInvoice/Model/OrderCollection.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * @package   Dyode 
+ * @package   Dyode
  * @author    Sooraj Sathyan
  */
 namespace Dyode\ArInvoice\Model;
+
+// use Magento\Framework\View\Element\Template\Context;
+use Dyode\ArInvoice\Helper\Data;
+// use Magento\Framework\Model\Context;
+// use Magento\Framework\Registry;
 
 class OrderCollection extends \Magento\Framework\Model\AbstractModel// implements \Magento\Framework\DataObject\IdentityInterface
 {   
@@ -12,29 +17,55 @@ class OrderCollection extends \Magento\Framework\Model\AbstractModel// implement
      **/
     protected $orderCollectionFactory;
 
+    protected $_pageFactory;
+    
+    protected $data;
+    
+    protected $_helper;
+
+	public function __construct(
+		\Magento\Framework\Model\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory,
+        \Dyode\ArInvoice\Helper\Data $helper,
+        \Magento\Framework\Registry $data
+    ) {
+		$this->helper = $helper;
+        $this->_pageFactory = $pageFactory;
+        $this->_helper = $helper;
+		return parent::__construct($context, $data);
+	}
+
     public function getSalesOrderCollection()
     {       
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $orderCollection = $objectManager->create('\Magento\Sales\Model\ResourceModel\Order\Collection');
         $orderCollection->load();
+        // $helperFactory = $objectManager->get('\Magento\Core\Model\Factory\Helper');
         // print_r($orderCollection->getData());
         foreach ($orderCollection as $order) {
             $paymentMethod = $order->getPayment()->getMethod();
-        //     // // $methodTitle = $method->getTitle();
-        //     print_r($order->getData());
-        //     // print_r($order->getPayment()->getMethod());
-        //     // print_r($methodTitle);
-        //     // echo " ";
+            // // $methodTitle = $method->getTitle();
+            // print_r($order->getData());
+            // print_r($order->getPayment()->getMethod());
+            // print_r($methodTitle);
+            // echo " ";
             if (strpos($paymentMethod, 'authorizenet') !== false) {
                 echo 'Authorize.net';
             } else {
                 echo 'false';
             }
             echo " ";
+            if (!empty($order->getCustomerId())) {
+                echo $this->_helper->validateAccountNumber($order->getCustomerId());
+                // var_dump($order->getCustomerId());    
+            } else {
+                echo "Account Number not found!";
+            }
         }
         die();
         // $collection = $this->_orderCollectionFactory->create()->addAttributeToSelect('*');
         // // $this->orderCollectionFactory->addFieldToFilter('status','complete');
         // return $collection;
-    }     
+    }
+    
 }
