dfda0074079bf7fa2222abeccce445a8d2af3d0f
nismath vi <nismath.vi@dyode.com>
2018-08-23 20:03:11 +0530

    classname added

--- app/code/Dyode/CategoryImageWidget/Block/Widget/CategoryImageWidget.php
+++ app/code/Dyode/CategoryImageWidget/Block/Widget/CategoryImageWidget.php
@@ -77,6 +77,7 @@ class CategoryImageWidget extends \Magento\Framework\View\Element\Template imple
 		/**
 		* function name : getClassName
      * Retrieve classname
+		 *
     */
 		public function getClassName()
 	  {
