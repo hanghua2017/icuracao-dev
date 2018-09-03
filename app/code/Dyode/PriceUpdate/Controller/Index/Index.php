c31cb5993a2e37e9a1798566f75d0c00f1ef12f9
nithindyode <nithin@dyode.com>
2018-09-03 12:52:51 +0530

    Price update module completed

--- app/code/Dyode/PriceUpdate/Controller/Index/Index.php
+++ app/code/Dyode/PriceUpdate/Controller/Index/Index.php
@@ -2,24 +2,26 @@
 
 namespace Dyode\PriceUpdate\Controller\Index;
 
+/**
+ * Price Update Contoller
+ * @category Dyode
+ * @package  Dyode_PriceUpdate
+ * @module   PriceUpdate
+ * @author   Nithin
+ */
 class Index extends \Magento\Framework\App\Action\Action
 {
     protected $_priceModel;
 
-    protected $_productCollectionFactory;
-
     public function __construct(  	
 	\Magento\Framework\App\Action\Context $context,  
-	\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory, 
 	\Dyode\PriceUpdate\Model\PriceUpdate $priceModel
 	) {
-	    $this->_productCollectionFactory = $productCollectionFactory;
 	    $this->_priceModel = $priceModel;
 	    parent::__construct($context);
 	}
 
 	public function execute(){
     	$this->_priceModel->updatePrice();
-        
     }
 }
--- app/code/Dyode/PriceUpdate/Model/PriceUpdate.php
+++ app/code/Dyode/PriceUpdate/Model/PriceUpdate.php
@@ -3,38 +3,48 @@ namespace Dyode\PriceUpdate\Model;
 
 use \Magento\Framework\Model\AbstractModel;
 
+/**
+ * Price Model
+ * @category Dyode
+ * @package  Dyode_PriceUpdate
+ * @module   PriceUpdate
+ * @author   Nithin
+ */
 class PriceUpdate extends \Magento\Framework\View\Element\Template {
 
 	protected $_productCollectionFactory;
 
-	protected $_curl;
-
-	public $data = array();
-
 	public $skuList = array();
 
-	public $location = '81';
+	public $skus = array();
 
 	public function __construct(
 	\Magento\Framework\View\Element\Template\Context $context,  
 	\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory, 
 	\Dyode\PriceUpdate\Helper\Data $helper,
+	\Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
 	array $data = []
 	) {
 	    $this->_productCollectionFactory = $productCollectionFactory;
 	    $this->_helper = $helper;
-	    $this->_curl = $curl;
+	    $this->productRepository = $productRepository;
 	    parent::__construct($context, $data);
 	}
 
 	public function updatePrice() {
 		//$this->getProductSkulist();
-		$this->getBatchPrice();
+		//$this->getBatchPrice();
+		$this->processBatchprice();
 		var_dump("expression");exit;
 
 	} 
 
-	//get Sku list of all products
+	/**
+     * function name : getProductSkulist
+     * definition : get sku and product id of all products
+     * @param int $storeViewId
+     * @return no return
+     */
 	public function getProductSkulist(){
 		$productCollection = $this->_productCollectionFactory->create();
 	    /** Apply filters here */
@@ -42,7 +52,6 @@ class PriceUpdate extends \Magento\Framework\View\Element\Template {
 
 	    foreach ($productCollection as $product){
 	        $sku = utf8_encode(strtoupper(trim($product->getSku())));
-			//$this->data[] = "$sku";
 			$this->skuList[$sku]['entity_id'] = $product->getId();
 	    }  
 	} 
@@ -61,11 +70,6 @@ class PriceUpdate extends \Magento\Framework\View\Element\Template {
 		$request->setUri('https://exchangeweb.lacuracao.com:2007/ws1/test/restapi/ecommerce/getPrices?top50=true');
 		$request->setMethod(\Zend\Http\Request::METHOD_GET);
 
-		$params = new \Zend\Stdlib\Parameters([
-		   'top50' => true
-		]);
-		//$request->setQuery($params);
-
 		$client = new \Zend\Http\Client();
 		$options = [
 		   'adapter'   => 'Zend\Http\Client\Adapter\Curl',
@@ -78,54 +82,72 @@ class PriceUpdate extends \Magento\Framework\View\Element\Template {
 		$response = $client->send($request);
 		$data = json_decode($response->getBody());
 		$list = $data->LIST;
-		echo '<pre>';
-		//var_dump($list);exit;
 		foreach ($list as $key) {
-			echo '<pre>';
-			echo $key->price->COST;
+            $obj = array( 
+              'storeprice' => $key->price->STOREPRICE,
+              'special_price' => $key->price->SPECIALPRICE,
+              'vendorrebate' => $key->price->VENDORREBATE,
+              'customerrebate' => $key->price->CUSTOMERREBATE,
+              'recycling_price' => $key->price->RECYCLEAMOUNT,
+              'recycling_description' => $key->price->RECYCLEINFO,
+              'cost' => $key->price->COST,
+              'msrp' => $key->price->MSRP,
+              'sku' => utf8_encode(trim( $key->item_id )),
+              'iqi' => trim( $key->price->IQI_STATUS ),
+              'ar_status' => trim( $key->price->ITEM_TYPE1 )	
+			);
+			$this->skus[ $obj['sku'] ]= $obj;
 		}
-
-		// $url = "https://exchangeweb.lacuracao.com:2007/ws1/test/restapi/ecommerce/getPrices?top50=true";
-		// $this->_curl->addHeader("Content-Type: application/json", "X-Api-Key: TEST-WNNxLUjBxA78J7s");
-		// $test = $this->_curl->get($url);
-		// $response = $this->_curl->getBody();
-		// var_dump($test);exit;
-		// $this->data[0] = '21 -N13-5000';
-		// $this->data[1] = '42C-F42-MARVISTA/6PC';
-		// $this->data[2] = '48B-M25-4501/BK';
-		// $this->data[3] = '21 -M35-HY/HME';
-		// $this->data[4] = '21B-N77-82471795';
-		// $items = array_chunk($this->data, 100);
-
-		// foreach ($items as $item) {
-		// 	$sku = implode(';', $item);
-		// 	$batchPrice = $this->_helper->batchGetPrice($sku,$this->location);
-
-		// 	$this->processBatchprice($batchPrice);
-		// }
-		// var_dump("expression");exit;
 	} 
 
-	public function processBatchprice($priceObject){
-		$pricelist = json_decode($priceObject->BatchGetPriceResult, true);
-		$items = $pricelist['SKULIST'];
-		foreach ($items as $item){
-				$obj = array( 
-					'err' => strtoupper(trim( $item[0] )),
-					'price' => $item[1],
-					'special_price' => $item[2],
-					'rebate' => $item[3],
-					'recycling_price' => $item[4],
-					'recycling_description' => trim( $item[5] ),
-					'cost' => $item[6],
-					'msrp' => $item[7],
-					'unknown' => $item[8],	
-					'sku' => utf8_encode(trim( $item[9] )),
-					'iqi' => trim( $item[10] ),
-					'ar_status' => trim( $item[11] )	
-				);
+	public function processBatchprice(){
 
-				$this->skus[ $obj['sku'] ]= $obj;
-			}
+		$special_to_date = date('Y-m-d', strtotime("+2 days"));
+		$special_from_date = date('Y-m-d', time());
+        foreach ($this->skus as $item) {
+            $entity_id = $this->skuList[ utf8_encode($item['sku']) ]['entity_id'];
+            if(!empty($entity_id)){
+            	$price = $item['storeprice'];
+			    $special_price = $item['special_price'];
+			    $vendor_rebate = $item['vendorrebate'];
+			    $customer_rebate = $item['customerrebate'];
+			    $recycling_price = $item['recycling_price'];
+			    $recycling_description = $item['recycling_description'];
+			    $cost = $item['cost'];
+			    $msrp = $item['msrp'];
+			    $sku = $item['sku'];
+			    $iqi = $item['iqi'];
+			    $ar_status = $item['ar_status'];
+
+                $rebated_price = $price - $customer_rebate;
+                $cost = $cost - $vendor_rebate;	
+                if (($special_price == 0) || ($rebated_price < $special_price)) { 
+                	$special_price = $rebated_price; 
+                }
+                if(0 < $special_price && $special_price < $price){
+                    $specialprice = $special_price;
+                    $specialfromdate = $special_from_date;
+                    $specialtodate = $special_to_date;                    
+                } else {
+                	$specialprice = '';
+                	$specialfromdate = '';
+                	$specialtodate = '';
+                }	
+                $product = $this->productRepository->getById($entity_id);
+                $product->setPrice($price);
+				$product->setSpecialPrice($specialprice);
+				$product->setCost($cost);
+				$product->setArStatus($ar_status);	
+				$product->setSpecialFromDate($specialfromdate);
+				$product->setSpecialToDate($specialtodate); 
+				$product->setMsrp($msrp); 
+				$product->setIqi($iqi);  
+				$product->setRecyclingPrice($recycling_price);  
+				$product->setRecyclingDescription($recycling_description); 
+				$product->setVendorRebate($vendor_rebate); 
+				$product->setCustomerRebate($customer_rebate); 
+				$product->save();
+            }            
+        }
 	}
 }
