d3a38c8d2247b96367bcd4b76f8a52f0511ca9e1
Sangeetha D <sangeetha.d@dyode.com>
2018-09-11 11:21:27 +0530

    changes

--- app/design/frontend/Dyode/curacao/web/js/onclick.js
+++ app/design/frontend/Dyode/curacao/web/js/onclick.js
@@ -1,16 +1,13 @@
+//Sign up onclick input show hide
 require([
   'jquery'
 ], function($){
-  // $('input[name="curacaocustid"]').hide();
-   $('input[name="curacaocustid"]').attr("placeholder", "Do you have a Curacao Account?");  
+  $('input[name="curacaocustid"]').attr("placeholder", "Do you have a Curacao Account?");
   $('.field-curacaocustid').hide();
-  //show it when the checkbox is clicked
   $('input[name="account-check"]').on('click', function () {
       if ($(this).prop('checked')) {
-          // $('input[name="curacaocustid"]').fadeIn();
           $('.field-curacaocustid').fadeIn();
       } else {
-          // $('input[name="curacaocustid"]').hide();
           $('.field-curacaocustid').hide();
       }
   });
