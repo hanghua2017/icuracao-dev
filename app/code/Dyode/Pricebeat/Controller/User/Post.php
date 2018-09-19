bebdeb0b782e10a4c87f0cb262253c9f050730b8
nismath vi <nismath.vi@dyode.com>
2018-09-19 10:21:11 +0530

    image url saved to db and folder

--- app/code/Dyode/Pricebeat/Controller/User/Post.php
+++ app/code/Dyode/Pricebeat/Controller/User/Post.php
@@ -7,23 +7,43 @@ use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Session\SessionManagerInterface;
 use Magento\Framework\HTTP\PhpEnvironment\Request;
+use \Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Request\Http;
+use \Dyode\Pricebeat\Model\Upload;
+use \Magento\Framework\Filesystem;
+use \Magento\MediaStorage\Model\File\UploaderFactory;
+
+use \Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use \Magento\Customer\Model\Session;
 
 class Post extends Action
 {
     protected $_modelFormFactory;
     protected $resultPageFactory;
     protected $_sessionManager;
+    protected  $fileUploaderFactory;
+    protected  $filesystem;
+
+    protected $customerSession;
 
 
     public function __construct(
       Context $context,
       FormFactory $modelFormFactory,
       PageFactory $pageFactory,
-      SessionManagerInterface $sessionManager)
+      SessionManagerInterface $sessionManager,
+      Filesystem $fileSystem,
+      Session $customerSession
+
+
+      )
     {
         $this->resultPageFactory = $pageFactory;
         $this->_modelFormFactory = $modelFormFactory;
         $this->_sessionManager = $sessionManager;
+        $this->fileSystem = $fileSystem;
+        $this->customerSession = $customerSession;
         parent::__construct($context);
     }
 
@@ -31,6 +51,7 @@ class Post extends Action
     {
         $this->validatedParams();
         $this->getFormData();
+
         echo "hello from the controller";
         exit();
     }
@@ -74,7 +95,31 @@ class Post extends Action
        $resultRedirect     = $this->resultRedirectFactory->create();
        $FormModel          = $this->_modelFormFactory->create();
        $data               = $this->getRequest()->getPost();
+       $filedata           = $this->getRequest()->getFiles('upload_file');
        $date               = date('Y-m-d h:i:sa');
+           if ($_FILES['upload_file']['name']) {
+               try {
+                   $uploader = $this->_objectManager->create(
+                       'Magento\MediaStorage\Model\File\Uploader',
+                       ['fileId' => 'upload_file']
+                   );
+                   $uploader->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
+                   $uploader->setAllowRenameFiles(true);
+                   $uploader->setFilesDispersion(true);
+
+                   $mediaDirectory = $this->fileSystem->getDirectoryRead(DirectoryList::MEDIA);
+                   $path = $mediaDirectory->getAbsolutePath('images/');
+                   $imagePath = $uploader->save($path);
+                   $data['upload_file'] = $imagePath['file'];
+                   // var_dump($data);
+                   // exit();
+               } catch (Exception $e) {
+                   $this->messageManager->addException($e->getMessage());
+                   return $resultRedirect->setUrl($this->_redirect->getRefererUrl());
+               }
+           }
+
+
 
        $FormModel->setData('first_name', $data['first_name']);
        $FormModel->setData('last_name', $data['last_name']);
@@ -84,8 +129,7 @@ class Post extends Action
        $FormModel->setData('invoice_number', $data['invoice_number']);
        $FormModel->setData('product_url', $data['product_url']);
        $FormModel->setData('product_image_url', $data['product_image_url']);
-
-
+       $FormModel->setData('product_image_url', $data['upload_file']);
        $FormModel->setData('created_date', $date);
        $FormModel->setData('status', $data['status']);
 
@@ -94,4 +138,6 @@ class Post extends Action
        $this->_redirect('blog/index');
        $this->messageManager->addSuccess(__('The data has been saved.'));
     }
+
+
 }
