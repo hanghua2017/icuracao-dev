3cdb95cf1375d42a0fedfc88457d813865b648ae
nismath vi <nismath.vi@dyode.com>
2018-09-19 20:49:14 +0530

    interest beat

---
+++ app/code/Dyode/Interestbeat/Block/Adminhtml/Form/Edit.php
@@ -0,0 +1,80 @@
+<?php
+namespace Dyode\Interestbeat\Block\Adminhtml\Form;
+
+use Magento\Backend\Block\Widget\Form\Container;
+
+class Edit extends Container
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+
+    /**
+     * @param \Magento\Backend\Block\Widget\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Widget\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+        $this->_coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Department edit block
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_objectId = 'form_id';
+        $this->_blockGroup = 'Dyode_Interestbeat';
+        $this->_controller = 'adminhtml_form';
+
+        parent::_construct();
+
+        $this->buttonList->remove('save');
+        $this->buttonList->remove('reset');
+        $this->buttonList->remove('delete');
+        $this->buttonList->remove('back');
+
+    }
+
+    /**
+     * Get header with Department name
+     *
+     * @return \Magento\Framework\Phrase
+     */
+    public function getHeaderText()
+    {
+        return __('Edit Product Faq');
+    }
+
+    /**
+     * Check permission for passed action
+     *
+     * @param string $resourceId
+     * @return bool
+     */
+    protected function _isAllowedAction($resourceId)
+    {
+        return $this->_authorization->isAllowed($resourceId);
+    }
+
+    /**
+     * Getter of url for "Save and Continue" button
+     * tab_id will be replaced by desired by JS later
+     *
+     * @return string
+     */
+    protected function _getSaveAndContinueUrl()
+    {
+        return $this->getUrl('productfaq/*/save', ['_current' => true, 'back' => 'edit', 'active_tab' => '']);
+    }
+}
---
+++ app/code/Dyode/Interestbeat/Block/Adminhtml/Form/Edit/Tab/Form.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Dyode\Interestbeat\Block\Adminhtml\Form\Edit\Tab;
+
+class Form extends \Magento\Backend\Block\Widget\Form\Generic implements \Magento\Backend\Block\Widget\Tab\TabInterface
+{
+    /**
+     * Country options
+     *
+     * @var \Magento\Config\Model\Config\Source\Yesno
+     */
+    protected $booleanOptions;
+
+    /**
+     * constructor
+     *
+     * @param \Magento\Config\Model\Config\Source\Yesno $booleanOptions
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Data\FormFactory $formFactory
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Config\Model\Config\Source\Yesno $booleanOptions,
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Data\FormFactory $formFactory,
+        array $data = []
+    )
+    {
+        $this->booleanOptions = $booleanOptions;
+        parent::__construct($context, $registry, $formFactory, $data);
+    }
+
+    /**
+     * Prepare form
+     *
+     * @return $this
+     */
+    protected function _prepareForm()
+    {
+        /** @var \Dyode\Interestbeat\Model\Form $form */
+        $model = $this->_coreRegistry->registry('dyode_dyode_form_data');
+
+        $form = $this->_formFactory->create();
+        $form->setHtmlIdPrefix('form_');
+        $form->setFieldNameSuffix('form');
+        $htmlIdPrefix = $form->getHtmlIdPrefix();
+        $fieldset = $form->addFieldset(
+            'base_fieldset',
+            [
+                'legend' => __('General Information'),
+                'class'  => 'fieldset-wide'
+            ]
+        );
+
+        if ($form->getId()) {
+            $fieldset->addField(
+                'form_id',
+                'hidden',
+                ['name' => 'form_id']
+            );
+        }
+        $fieldset->addField(
+          'first_name',
+          'label', [
+          'name' => 'first_name',
+          'label' => 'First Name',
+          ]
+          );
+
+          $fieldset->addField(
+          'last_name',
+          'label', [
+          'name' => 'last_name',
+          'label' => 'Last Name',
+          ]
+          );
+
+          $fieldset->addField(
+          'email',
+          'label', [
+          'name' => 'email',
+          'label' => 'Email',
+          ]
+          );
+
+          $fieldset->addField(
+          'phonenumber',
+          'label', [
+          'name' => 'phonenumber',
+          'label' => 'Phonenumber',
+          ]
+          );
+          $fieldset->addField(
+          'account_number',
+          'label', [
+          'name' => 'account_number',
+          'label' => 'Account Number',
+          ]
+          );
+          $fieldset->addField(
+          'invoice_number',
+          'label', [
+          'name' => 'invoice_number',
+          'label' => 'Invoice',
+          ]
+          );
+          $fieldset->addField(
+          'product_url',
+          'label', [
+          'name' => 'product_url',
+          'label' => 'Url',
+          ]
+          );
+          // $fieldset->addField(
+          // 'product_image_url',
+          // 'label', [
+          // 'name' => 'product_image_url',
+          // 'label' => 'Image',
+          // ]
+          // );
+
+        $form->setValues($model->getData());
+        $this->setForm($form);
+
+        return parent::_prepareForm();
+    }
+
+    /**
+     * Prepare label for tab
+     *
+     * @return string
+     */
+    public function getTabLabel()
+    {
+        return __('Interestbeat');
+    }
+
+    /**
+     * Prepare title for tab
+     *
+     * @return string
+     */
+    public function getTabTitle()
+    {
+        return $this->getTabLabel();
+    }
+
+    /**
+     * Can show tab in tabs
+     *
+     * @return boolean
+     */
+    public function canShowTab()
+    {
+        return true;
+    }
+
+    /**
+     * Tab is hidden
+     *
+     * @return boolean
+     */
+    public function isHidden()
+    {
+        return false;
+    }
+}
