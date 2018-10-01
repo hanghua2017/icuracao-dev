376d5aefcc979542797a516720d95272ab3c69b7
Kavitha <kavitha@dyode.com>
2018-10-01 13:31:11 +0530

    for checking whther the account is already linked

--- app/code/Dyode/Checkout/Model/ConfigProvider.php
+++ app/code/Dyode/Checkout/Model/ConfigProvider.php
@@ -13,7 +13,6 @@ class ConfigProvider implements ConfigProviderInterface
 {
    /** @var LayoutInterface  */
    protected $_layout;
-   protected $_cmsBlock;
    protected $_customerSession;
    protected $_helper;
    protected $_customerRepositoryInterface;
@@ -21,6 +20,7 @@ class ConfigProvider implements ConfigProviderInterface
    protected $_cart;
    protected $_priceHelper;
    protected $_canApply;
+   protected $_linked;
 
    public function __construct(\Magento\Framework\Pricing\Helper\Data $priceHelper,\Magento\Customer\Model\Session $customerSession,\Magento\Checkout\Model\Cart $cart, \Dyode\ARWebservice\Helper\Data $helper,\Magento\Customer\Api\CustomerRepositoryInterface $customerRepositoryInterface, LayoutInterface $layout, $blockId)
    {
@@ -28,53 +28,67 @@ class ConfigProvider implements ConfigProviderInterface
        $this->_layout = $layout;
        $this->_helper = $helper;
        $this->_customerRepositoryInterface = $customerRepositoryInterface;
-       $this->_cmsBlock = $this->constructBlock($blockId);
        $this->_cart = $cart;
        $this->_priceHelper = $priceHelper;
    }
 
-   public function constructBlock($blockId){
-       $block = $this->_layout->createBlock('Magento\Cms\Block\Block')
-           ->setBlockId($blockId)->toHtml();
-       return $block;
-   }
-
    public function getConfig()
    {
-      $configArr ['cms_block'] = $this->_cmsBlock;
-      $configArr['canapply'] = $this->_canApply;
-      $configArr['limit'] = $this->getLimit();
-      $configArr['total']= $this->getDownPayment();
+      $configArr['cms_block']   =   $this->_cmsBlock;
+      $configArr['canapply']    =   $this->_canApply;
+      $configArr['limit']       =   $this->getLimit();
+      $configArr['total']       =   $this->getDownPayment();
+      $configArr['linked']      =   $this->_linked;
       return $configArr;
    }
 
     public function getLimit(){
-       if (!$this->_customerSession->getCustomerId()) {
-           return false;
-       }
-      $customerId = $this->_customerSession->getCustomerId();
-      //$customer = $this->_customerRepositoryInterface->getById($customerId);
-      //$curaAccId = $customer->getCuracaocustid();
-      $curaAccId = '54420730';
-      $this->_curacaoId = $curaAccId;
-    //  $result = $this->_helper->getCreditLimit($curaAccId);
-    //  $limit = (float)$result->CREDITLIMIT;
-      $limit = 500.00;
-      $this->_canApply = 1;
-      $formattedCurrencyValue = $this->_priceHelper->currency($limit, true, false);
-      return $formattedCurrencyValue;
+        $curaAccId =  $this->getCuracaoId();
+            if($curaAccId){
+                $this->_curacaoId = $curaAccId;
+                $result = $this->_helper->getCreditLimit($curaAccId);
+                $limit = (float)$result->CREDITLIMIT;
+                $this->_canApply = 1;
+                $formattedCurrencyValue = $this->_priceHelper->currency($limit, true, false);
+                return $formattedCurrencyValue;
+            }
+            return false;
    }
 
    public function getDownPayment(){
-    //  $curaAccId = $this->_curacaoId;
-      $curaAccId = '54420740';
-    	$subTotal = $this->_cart->getQuote()->getSubtotal();
-      $params = array('cust_id'=>$curaAccId,'amount'=>$subTotal);
-    //  $result = $this->_helper->verifyPersonalInfm($params);
-      $result = 10.00;
-      $this->_customerSession->setDownPayment($result);
-      $formattedCurrencyValue = $this->_priceHelper->currency($result, true, false);
-      return $formattedCurrencyValue;
+        $curaAccId =  $this->getCuracaoId();
+        if($curaAccId){
+            $subTotal = $this->_cart->getQuote()->getSubtotal();
+            $params = array('cust_id'=>$curaAccId,'amount'=>$subTotal);
+            $result = $this->_helper->verifyPersonalInfm($params);
+            if($result)
+                $this->_customerSession->setDownPayment($result);
+            else
+                $result = 0;
+            $formattedCurrencyValue = $this->_priceHelper->currency($result, true, false);
+            return $formattedCurrencyValue;
+        }
+        return false;    	   
+    }
+   public function getCuracaoId(){
+        if (!$this->_customerSession->getCustomerId()) {
+            return false;
+        }
+        $customerId = $this->_customerSession->getCustomerId();
+        if($customerId){
+            $customer = $this->_customerRepositoryInterface->getById($customerId);
+            $curaAccId = $customer->getCuracaocustid();
+            if($curaAccId){
+                $this->_linked = true;
+                return $curaAccId;
+            } else {
+                $this->_linked = false;
+                return false;
+            }
+            
+        }
+        return false;
    }
+
 }
 ?>
