ffc7f416942ba7a2248759960a84adb223e81cdd
Kavitha <kavitha@dyode.com>
2018-10-03 17:43:13 +0530

    [update][checkout]mixins for showing checkout steps properlty

--- app/code/Dyode/DeliveryMethod/view/base/requirejs-config.js
+++ app/code/Dyode/DeliveryMethod/view/base/requirejs-config.js
@@ -1,12 +1,15 @@
 var config = {
- 'config': {
-     'mixins': {
-        'Dyode_Checkout/js/view/shipping': {
-           'Dyode_DeliveryMethod/js/view/shipping-payment-mixin': true
-           },
-        'Magento_Checkout/js/view/payment': {
-            'Dyode_DeliveryMethod/js/view/shipping-payment-mixin': true
-           }
+    'config': {
+        'mixins': {
+            'Dyode_ShippingStep/js/view/shipping-method-step': {
+                'Dyode_DeliveryMethod/js/view/checkout-steps-mixin': true
+            },
+            'Dyode_Checkout/js/view/shipping': {
+                'Dyode_DeliveryMethod/js/view/checkout-steps-mixin': true
+            },
+            'Magento_Checkout/js/view/payment': {
+                'Dyode_DeliveryMethod/js/view/checkout-steps-mixin': true
+            }
+        }
     }
- }
 }
