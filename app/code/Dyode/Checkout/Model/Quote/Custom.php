afbef565d76e5b65f18c7cd8509779b2ac6c661a
rktdyode <rajeev.ktomy@dyode.com>
2018-10-04 16:34:34 +0530

    [FIX][CHECKOUT]curacao-id checking issue fix

--- app/code/Dyode/Checkout/Model/Quote/Custom.php
+++ app/code/Dyode/Checkout/Model/Quote/Custom.php
@@ -7,94 +7,109 @@
 
 namespace Dyode\Checkout\Model\Quote;
 /**
-* Class Custom
-* @package Dyode\Checkout\Model\Quote
-*/
+ * Class Custom
+ *
+ * @package Dyode\Checkout\Model\Quote
+ */
 class Custom extends \Magento\Quote\Model\Quote\Address\Total\AbstractTotal
 {
-  /**
-  * @var \Magento\Framework\Pricing\PriceCurrencyInterface
-  */
-  protected $_priceCurrency;
-  protected $_customerSession;
-  protected $_curacaocredit;
-  /**
-   * @param \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency [description]
-   */
-  public function __construct(
-      \Magento\Customer\Model\Session $customerSession,
-      \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency
-  ) {
-      $this->_customerSession = $customerSession;
-      $this->_priceCurrency = $priceCurrency;
-  }
+    /**
+     * @var \Magento\Framework\Pricing\PriceCurrencyInterface
+     */
+    protected $_priceCurrency;
 
-  public function collect(
-      \Magento\Quote\Model\Quote $quote,
-      \Magento\Quote\Api\Data\ShippingAssignmentInterface $shippingAssignment,
-      \Magento\Quote\Model\Quote\Address\Total $total
-  ) {
-      parent::collect($quote, $shippingAssignment, $total);
-      $address = $shippingAssignment->getShipping()->getAddress();
-      $customDiscount  = $downPayment = $discount = 0;
-      $curaAccId = '';
-       if($address->getAddressType() != 'billing'){
-           return $this;
-       }
-        if(!$this->_customerSession->isLoggedIn()){
-          return $this;
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $_customerSession;
+
+    /**
+     * @var int
+     */
+    protected $_curacaocredit;
+
+    /**
+     * @param \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency [description]
+     */
+    public function __construct(
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency
+    ) {
+        $this->_customerSession = $customerSession;
+        $this->_priceCurrency = $priceCurrency;
+    }
+
+    /**
+     * @param \Magento\Quote\Model\Quote $quote
+     * @param \Magento\Quote\Api\Data\ShippingAssignmentInterface $shippingAssignment
+     * @param \Magento\Quote\Model\Quote\Address\Total $total
+     * @return $this|\Magento\Quote\Model\Quote\Address\Total\AbstractTotal
+     */
+    public function collect(
+        \Magento\Quote\Model\Quote $quote,
+        \Magento\Quote\Api\Data\ShippingAssignmentInterface $shippingAssignment,
+        \Magento\Quote\Model\Quote\Address\Total $total
+    ) {
+        parent::collect($quote, $shippingAssignment, $total);
+        $address = $shippingAssignment->getShipping()->getAddress();
+        $customDiscount = $downPayment = $discount = 0;
+        $curaAccId = '';
+        if ($address->getAddressType() != 'billing') {
+            return $this;
+        }
+        if (!$this->_customerSession->isLoggedIn()) {
+            return $this;
         }
 
-        if($quote->getCustomer()->getCustomAttribute('curacaocustid')){
-
+        if ($quote->getCustomer()->getCustomAttribute('curacaocustid') != null) {
             $curaAccId = $quote->getCustomer()->getCustomAttribute('curacaocustid')->getValue();
-            $downPayment = $this->_customerSession->getDownPayment()? $this->_customerSession->getDownPayment() : 0;
+            $downPayment = $this->_customerSession->getDownPayment() ? $this->_customerSession->getDownPayment() : 0;
             $this->_curacaocredit = 0;
 
-            if(!($curaAccId)){
+            if (!($curaAccId)) {
                 return $this;
             }
             if ($quote->getCustomer()->getId() && $curaAccId != 0) {
-          
-                if ($quote->getUseCredit() && $downPayment>0) {
-                  //Calculate the discount
-                  $subTotal  = $quote->getSubtotal();
-                  $discount = $subTotal - $downPayment;
-                  $customDiscount = -$discount;
-                  $this->_curacaocredit = -$discount;
-      
-                  $total->addTotalAmount('customdiscount', $customDiscount);
-                  $total->addBaseTotalAmount('customdiscount', $customDiscount);
-                  $quote->setCuracaocreditUsed($this->_curacaocredit);
+                if ($quote->getUseCredit() && $downPayment > 0) {
+                    //Calculate the discount
+                    $subTotal = $quote->getSubtotal();
+                    $discount = $subTotal - $downPayment;
+                    $customDiscount = -$discount;
+                    $this->_curacaocredit = -$discount;
+
+                    $total->addTotalAmount('customdiscount', $customDiscount);
+                    $total->addBaseTotalAmount('customdiscount', $customDiscount);
+                    $quote->setCuracaocreditUsed($this->_curacaocredit);
                 }
             }
         }
-      return $this;
-   }
+        return $this;
+    }
 
-  /**
-   * Assign subtotal amount and label to address object
-   *
-   * @param \Magento\Quote\Model\Quote $quote
-   * @param Address\Total $total
-   * @return array
-   * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-   */
-  public function fetch(\Magento\Quote\Model\Quote $quote, \Magento\Quote\Model\Quote\Address\Total $total)
-  {
-      return [
-          'code'  => 'Custom_Discount',
-          'title' => $this->getLabel(),
-          'value' => $this->_curacaocredit
-      ];
-  }
+    /**
+     * Assign subtotal amount and label to address object
+     *
+     * @param \Magento\Quote\Model\Quote $quote
+     * @param Address\Total $total
+     * @return array
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function fetch(\Magento\Quote\Model\Quote $quote, \Magento\Quote\Model\Quote\Address\Total $total)
+    {
+        return [
+            'code'  => 'Custom_Discount',
+            'title' => $this->getLabel(),
+            'value' => $this->_curacaocredit,
+        ];
+    }
 
-  /**
-   * get label
-   * @return string
-   */
-  public function getLabel()
-  {
-      return __('Curacao Credit');
-  }
+    /**
+     * get label
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        return __('Curacao Credit');
+    }
 }
