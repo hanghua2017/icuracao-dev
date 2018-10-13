19b5ece7efb6ac1fe00a233834f63b955c8cd0b7
rktdyode <rajeev.ktomy@dyode.com>
2018-10-13 17:54:35 +0530

    [modify][chekout]shipping-info api - dealing the carrier_code cannot be null exception handling

--- app/code/Dyode/Checkout/Model/GuestShippingInfoProcessor.php
+++ app/code/Dyode/Checkout/Model/GuestShippingInfoProcessor.php
@@ -13,6 +13,7 @@ namespace Dyode\Checkout\Model;
 
 use Dyode\Checkout\Api\GuestShippingInfoInterface;
 use Dyode\Checkout\Api\Data\ShippingInformationInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 /**
  * Guest ShippingInfo Processor
@@ -32,17 +33,33 @@ class GuestShippingInfoProcessor implements GuestShippingInfoInterface
      */
     protected $shippingInformationManagement;
 
+    protected $paymentMethodManagement;
+
+    protected $paymentDetailsFactory;
+
+    protected $cartTotalsRepository;
+
     /**
+     * GuestShippingInfoProcessor constructor.
+     *
      * @param \Magento\Quote\Model\QuoteIdMaskFactory $quoteIdMaskFactory
      * @param \Magento\Checkout\Api\ShippingInformationManagementInterface $shippingInformationManagement
-     * @codeCoverageIgnore
+     * @param \Magento\Quote\Api\PaymentMethodManagementInterface $paymentMethodManagement
+     * @param \Magento\Checkout\Model\PaymentDetailsFactory $paymentDetailsFactory
+     * @param \Magento\Quote\Api\CartTotalRepositoryInterface $cartTotalsRepository
      */
     public function __construct(
         \Magento\Quote\Model\QuoteIdMaskFactory $quoteIdMaskFactory,
-        \Magento\Checkout\Api\ShippingInformationManagementInterface $shippingInformationManagement
+        \Magento\Checkout\Api\ShippingInformationManagementInterface $shippingInformationManagement,
+        \Magento\Quote\Api\PaymentMethodManagementInterface $paymentMethodManagement,
+        \Magento\Checkout\Model\PaymentDetailsFactory $paymentDetailsFactory,
+        \Magento\Quote\Api\CartTotalRepositoryInterface $cartTotalsRepository
     ) {
         $this->quoteIdMaskFactory = $quoteIdMaskFactory;
         $this->shippingInformationManagement = $shippingInformationManagement;
+        $this->paymentMethodManagement = $paymentMethodManagement;
+        $this->paymentDetailsFactory = $paymentDetailsFactory;
+        $this->cartTotalsRepository = $cartTotalsRepository;
     }
 
     /**
@@ -54,9 +71,28 @@ class GuestShippingInfoProcessor implements GuestShippingInfoInterface
     ) {
         /** @var $quoteIdMask \Magento\Quote\Model\QuoteIdMask */
         $quoteIdMask = $this->quoteIdMaskFactory->create()->load($cartId, 'masked_id');
-        return $this->shippingInformationManagement->saveAddressInformation(
-            $quoteIdMask->getQuoteId(),
-            $addressInformation
-        );
+
+        try {
+            $this->shippingInformationManagement->saveAddressInformation(
+                $quoteIdMask->getQuoteId(),
+                $addressInformation
+            );
+        } catch (NoSuchEntityException $exception) {
+            /**
+             * since the carrier_code and method_code are null, we are expecting this exception here.
+             * No worries.. let's continue here.
+             */
+            /** @var \Magento\Checkout\Api\Data\PaymentDetailsInterface $paymentDetails */
+            $paymentDetails = $this->paymentDetailsFactory->create();
+            $paymentDetails->setPaymentMethods($this->paymentMethodManagement->getList($quoteIdMask->getQuoteId()));
+            $paymentDetails->setTotals($this->cartTotalsRepository->get($quoteIdMask->getQuoteId()));
+            return $paymentDetails;
+        }
+
+        /** @var \Magento\Checkout\Api\Data\PaymentDetailsInterface $paymentDetails */
+        $paymentDetails = $this->paymentDetailsFactory->create();
+        $paymentDetails->setPaymentMethods($this->paymentMethodManagement->getList($quoteIdMask->getQuoteId()));
+        $paymentDetails->setTotals($this->cartTotalsRepository->get($quoteIdMask->getQuoteId()));
+        return $paymentDetails;
     }
 }
