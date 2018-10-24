cd7f548305c6ff515e0fc4c04d571686ca83e7d6
rktdyode <rajeev.ktomy@dyode.com>
2018-10-24 16:20:15 +0530

    [FIX]Reverting unexpectedly ovverrided files

--- app/design/frontend/Dyode/curacao/requirejs-config.js
+++ app/design/frontend/Dyode/curacao/requirejs-config.js
@@ -1,33 +1,29 @@
 var config = {
-    "paths": {
-      'accordion':'js/cms-accordion',
-      'checkbox':'js/onclick',
-      'customjs':'js/custom',
-      'custom-select':'js/custom-select'
+    'paths': {
+        'accordion': 'js/cms-accordion',
+        'checkbox': 'js/onclick',
+        'customjs': 'js/custom',
+        'custom-select': 'js/custom-select'
     },
-    "shim":{
-      'accordion': {
-          'deps': ['jquery']
-      },
-      'checkbox': {
-          'deps': ['jquery']
-      },
-      'custom-select': {
-          'deps': ['jquery']
-      },
-      'customjs': {
-          'deps': ['jquery']
-      }
+    'shim': {
+        'accordion': {
+            'deps': ['jquery']
+        },
+        'checkbox': {
+            'deps': ['jquery']
+        },
+        'custom-select': {
+            'deps': ['jquery']
+        },
+        'customjs': {
+            'deps': ['jquery']
+        }
+    },
+    'config': {
+        'mixins': {
+            'Magento_Catalog/js/price-utils': {
+                'js/price-utils-mixin': true
+            }
+        }
     }
 };
-
-// var config = {
-//     'config': {
-//         'mixins': {
-//             'Magento_Catalog/js/price-utils': {
-//                 'js/price-utils-mixin': true
-//             }
-//         }
-//     }
-// };
-
