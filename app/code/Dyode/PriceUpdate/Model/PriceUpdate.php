3e1e1475090665fdb1e6c1bb79831586996adc54
nithindyode <nithin@dyode.com>
2018-10-26 12:27:41 +0530

    code changes

--- app/code/Dyode/PriceUpdate/Model/PriceUpdate.php
+++ app/code/Dyode/PriceUpdate/Model/PriceUpdate.php
@@ -32,18 +32,15 @@ class PriceUpdate extends \Magento\Framework\Model\AbstractModel
      * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
      */
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
         \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
         Data $apiHelper,
-        \Dyode\PriceUpdate\Helper\Data $priceHelper,
-        $data = []
+        \Dyode\PriceUpdate\Helper\Data $priceHelper
     ) {
         $this->productCollectionFactory = $productCollectionFactory;
         $this->productRepository = $productRepository;
         $this->apiHelper = $apiHelper;
         $this->helper = $priceHelper;
-        parent::__construct($context, $data);
     }
 
     /**
