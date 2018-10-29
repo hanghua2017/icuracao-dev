569746b3f7c4824abd82d2fd46c9f359c87fac04
rktdyode <rajeev.ktomy@dyode.com>
2018-10-29 18:46:47 +0530

    [CODE_OPTIMIZATION]Dyode_Threshold module

--- app/code/Dyode/Threshold/Controller/Index/Index.php
+++ app/code/Dyode/Threshold/Controller/Index/Index.php
@@ -2,25 +2,49 @@
 
 namespace Dyode\Threshold\Controller\Index;
 
-class Index extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Dyode\Threshold\Model\Threshold;
+
+class Index extends Action
 {
-	protected $_pageFactory;
 
-	public function __construct(
-		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory,
-		\Dyode\Threshold\Model\Threshold $thresholdModel)
-	{
-		$this->thresholdModel = $thresholdModel;
-		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context);
-	}
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
 
-	public function execute()
-	{
-		
-		$this->thresholdModel->getThreshold();
-	}
+    /**
+     * @var \Dyode\Threshold\Model\Threshold
+     */
+    protected $thresholdModel;
+
+    /**
+     * Index constructor.
+     *
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $pageFactory
+     * @param \Dyode\Threshold\Model\Threshold $thresholdModel
+     */
+    public function __construct(Context $context, PageFactory $pageFactory, Threshold $thresholdModel)
+    {
+        $this->thresholdModel = $thresholdModel;
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    /**
+     * Main entry point.
+     *
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     * @throws \Exception
+     */
+    public function execute()
+    {
+
+        $this->thresholdModel->getThreshold();
+    }
 
 }
 
--- app/code/Dyode/Threshold/Model/Threshold.php
+++ app/code/Dyode/Threshold/Model/Threshold.php
@@ -1,42 +1,65 @@
 <?php
+
 namespace Dyode\Threshold\Model;
 
-use \Magento\Framework\Model\AbstractModel;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\File\Csv;
 
-class Threshold extends \Magento\Framework\Model\AbstractModel {
+class Threshold
+{
 
-   public function __construct( 
-		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-		\Magento\Framework\File\Csv $csv
-	) {
-	    $this->scopeConfig = $scopeConfig;
-	    $this->csv = $csv;
-	}
+    const FILE_UPLOAD_CONFIG_PATH = 'dyode_threshold_section/threshold_group/threshold_file_upload';
 
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
 
-	public function getThreshold(){
+    /**
+     * @var \Magento\Framework\File\Csv
+     */
+    protected $csv;
 
-		$value = $this->scopeConfig->getValue('dyode_threshold_section/threshold_group/threshold_file_upload', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    /**
+     * Threshold constructor.
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\File\Csv $csv
+     */
+    public function __construct(ScopeConfigInterface $scopeConfig, Csv $csv)
+    {
+        $this->scopeConfig = $scopeConfig;
+        $this->csv = $csv;
+    }
 
-		$file = 'pub/media/uploads/'.$value;
+    /**
+     * Get threshold data
+     *
+     * @return array|bool
+     * @throws \Exception
+     */
+    public function getThreshold()
+    {
+        $value = $this->scopeConfig->getValue(self::FILE_UPLOAD_CONFIG_PATH, ScopeInterface::SCOPE_STORE);
+        $file = 'pub/media/uploads/' . $value;
+        $data = $this->csv->getData($file);
 
-      	$data = $this->csv->getData($file);
         //first line are the headers
-        if($data)
-        {	
-	        $headers = $data[0];
+        if ($data) {
+            $headers = $data[0];
 
-	        $allThresholdData = array();
-	        for($i=1; $i<count($data); $i++){
-	           $threshold = array();
-	        foreach ($headers as $index=>$attributeCode) {
-	           $threshold[$attributeCode] = $data[$i][$index];
-	        }
-	           $allThresholdData[] = $threshold;
-			        
-			}
-		   return $allThresholdData;
-		}
-		return false;
+            $allThresholdData = [];
+            for ($i = 1; $i < count($data); $i++) {
+                $threshold = [];
+                foreach ($headers as $index => $attributeCode) {
+                    $threshold[$attributeCode] = $data[$i][$index];
+                }
+                $allThresholdData[] = $threshold;
+
+            }
+            return $allThresholdData;
+        }
+        return false;
     }
 }
