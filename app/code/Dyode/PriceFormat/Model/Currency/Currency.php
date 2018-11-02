2cf4589fac54cecd97ae6ecff2a79cd18c2df5bb
nithindyode <nithin@dyode.com>
2018-11-02 18:49:42 +0530

    Currency Fix

--- app/code/Dyode/PriceFormat/Model/Currency/Currency.php
+++ app/code/Dyode/PriceFormat/Model/Currency/Currency.php
@@ -28,6 +28,7 @@ class Currency extends \Magento\Directory\Model\Currency
          * %f - the argument is treated as a float, and presented as a floating-point number (locale aware).
          * %F - the argument is treated as a float, and presented as a floating-point number (non-locale aware).
          */
+        $options["locale"] = 'en';
         $price = sprintf("%F", $price);
         return $this->_localeCurrency->getCurrency($this->getCode())->toCurrency($price, $options);
     }
