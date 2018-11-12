02dc2ce6a72ecb75907a57465883833a446968eb
nithindyode <nithin@dyode.com>
2018-11-12 14:59:28 +0530

    deleted unwanted files

--- app/code/Dyode/Test/Block/Index.php
+++
@@ -1,8 +0,0 @@
-<?php   
-class Dyode_Test_Block_Index extends Mage_Core_Block_Template{   
-
-
-
-
-
-}
--- app/code/Dyode/Test/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Dyode_Test_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-	 
