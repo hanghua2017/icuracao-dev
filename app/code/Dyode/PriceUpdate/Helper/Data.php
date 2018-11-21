851b2125f79e9b016dadf43e521b16142840b925
Kavitha <kavitha@dyode.com>
2018-11-21 18:54:25 +0530

    Price update issue fixed

--- app/code/Dyode/PriceUpdate/Helper/Data.php
+++ app/code/Dyode/PriceUpdate/Helper/Data.php
@@ -4,8 +4,16 @@
  */
 namespace Dyode\PriceUpdate\Helper;
 
+use Magento\Framework\Serialize\Serializer\Json;
+use Zend\Http\Request;
+use Zend\Http\Headers;
+use Zend\Http\Client;
+use Dyode\AuditLog\Model\ResourceModel\AuditLog;
+use Dyode\ARWebservice\Helper\Data as ARWebserviceHelper;
+
 /**
  * Price Helper
+ *
  * @category Dyode
  * @package  Dyode_PriceUpdate
  * @module   PriceUpdate
@@ -14,73 +22,225 @@ namespace Dyode\PriceUpdate\Helper;
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
 
-	/*
-	 * helper constructer
-	 */ 
-	public function __construct(
-	   	\Dyode\AuditLog\Model\ResourceModel\AuditLog $auditLog,	
-      	\Dyode\ARWebservice\Helper\Data $apiHelper 
-    ) {
-      $this->apiHelper = $apiHelper;
-      $this->auditLog = $auditLog;
+    /**
+     * @var \Dyode\ARWebservice\Helper\Data
+     */
+    protected $apiHelper;
+
+    /**
+     * @var \Dyode\AuditLog\Model\ResourceModel\AuditLog
+     */
+    protected $auditLog;
+
+    /**
+     * @var \Zend\Http\Request
+     */
+    protected $request;
+
+    /**
+     * @var \Zend\Http\Client
+     */
+    protected $client;
+
+    /**
+     * @var \Magento\Framework\Serialize\Serializer\Json
+     */
+    protected $jsonHelper;
+
+    /**
+     * @var array
+     */
+    protected $priceInfo = [];
+
+    /**
+     * Data constructor.
+     *
+     * @param \Dyode\AuditLog\Model\ResourceModel\AuditLog $auditLog
+     * @param \Dyode\ARWebservice\Helper\Data $apiHelper
+     * @param \Magento\Framework\Serialize\Serializer\Json $jsonHelper
+     */
+    public function __construct(AuditLog $auditLog, ARWebserviceHelper $apiHelper, Json $jsonHelper)
+    {
+        $this->apiHelper = $apiHelper;
+        $this->auditLog = $auditLog;
+        $this->jsonHelper = $jsonHelper;
     }
 
     /**
-     * function name : setStock
-     * definition : api call to set stock in AR
-     * @return no return
+     * Api call to set stock in AR
+     *
+     * @param array $productSkuList
+     * @param string $module
+     * @return bool
      */
-    public function setStock($productSkuList, $module)
+    public function sendSetStockARWebserviceRequest($productSkuList, $module)
     {
-    	$apiKey = $this->apiHelper->getApiKey();
-      	$apiUrl = $this->apiHelper->getApiUrl();
-      	$postString = array('list' => $productSkuList);
+        $postString = ['list' => $productSkuList];
+        $request = $this->prepareRequest('SetEcommerceStock', Request::METHOD_PUT);
+        $client = $this->prepareClient();
+        $request->setContent(json_encode($postString));
+
         try {
-
-            $httpHeaders = new \Zend\Http\Headers();
-            $httpHeaders->addHeaders([
-               'Accept' => 'application/json',
-               'Content-Type' => 'application/json',
-               'X-Api-Key' => $apiKey
-            ]);
-            $request = new \Zend\Http\Request();
-            $request->setHeaders($httpHeaders);
-            $request->setUri($apiUrl."SetEcommerceStock");
-            $request->setMethod(\Zend\Http\Request::METHOD_PUT);
-
-            $client = new \Zend\Http\Client();
-            $options = [
-               'adapter'   => 'Zend\Http\Client\Adapter\Curl',
-               'curloptions' => [CURLOPT_FOLLOWLOCATION => true],
-               'maxredirects' => 0,
-               'timeout' => 360
-            ];
-            $client->setOptions($options);
-  			    $request->setContent(json_encode($postString));
-            $this->addLogs('SetEcommerceStock API calling', $request, $module);
+            $this->addLogs('SetEcommerceStock API request sending', $request, $module);
             $response = $client->send($request);
-            $this->addLogs('SetEcommerceStock API calling', $response->getBody(), $module);
-        }
-        catch (\Exception $e) {
-        	$this->addLogs('SetEcommerceStock API calling', 'response failed '.$e, $module);
+            $this->addLogs('SetEcommerceStock API response success', $response->getBody(), $module);
+
+            return false;
+
+        } catch (\Exception $e) {
+            $this->addLogs('SetEcommerceStock API failed', 'response failed ' . $e, $module);
+
             return false;
         }
     }
 
     /**
-     * function name : addLogs
-     * definition : write audit logs
-     * @return no return
+     * get price details of all products from AR
+     *
+     * @param string $module
+     * @return array|bool
+     */
+    public function sendUpdatePriceARWebserviceRequest($module)
+    {
+        $request = $this->prepareRequest('getPrices?top50=false', Request::METHOD_GET);
+        $client = $this->prepareClient();
+        try {
+            $this->addLogs('getPrice API sending', $request, $module);
+            $response = $client->send($request);
+            $data = $this->jsonHelper->unserialize($response->getBody());
+
+            if (!$data || !isset($data['LIST'])) {
+                $this->addLogs('getPrice API failed', 'Repsonse: NULL', 'dyode_priceupdate');
+
+                return false;
+            }
+
+            foreach ($data['LIST'] as $listItem) {
+                $itemPrice = $listItem['price'];
+                $obj = [
+                    'storeprice'            => $itemPrice['STOREPRICE'],
+                    'special_price'         => $itemPrice['SPECIALPRICE'],
+                    'vendorrebate'          => $itemPrice['VENDORREBATE'],
+                    'recycling_price'       => $itemPrice['RECYCLEAMOUNT'],
+                    'recycling_description' => $itemPrice['RECYCLEINFO'],
+                    'cost'                  => $itemPrice['COST'],
+                    'msrp'                  => $itemPrice['MSRP'],
+                    'sku'                   => utf8_encode(trim($listItem['item_id'])),
+                    'iqi'                   => trim($itemPrice['IQI_STATUS']),
+                    'ar_status'             => trim($itemPrice['ITEM_TYPE1']),
+                ];
+                $this->priceInfo[$obj['sku']] = $obj;
+            }
+            $this->addLogs('getPrice API success', $response->getBody(), 'dyode_priceupdate');
+            if ($data['CONTINUE']) {
+                $this->sendUpdatePriceARWebserviceRequest();
+            }
+            $this->addLogs('getPrice API success', $response->getBody(), 'dyode_priceupdate');
+
+            return $this->priceInfo;
+
+        } catch (\Exception $e) {
+            $this->addLogs('getPrice API failed', 'connection failed' . $e, 'dyode_priceupdate');
+
+            return false;
+        }
+    }
+
+    /**
+     * Write audit logs
+     *
+     * @param string $action
+     * @param string $description
+     * @param string $module
+     * @return $this
      */
     public function addLogs($action, $description, $module)
     {
-    	$clientIP = "";
-	    $this->auditLog->saveAuditLog([
-            'user_id' => 'admin',
-            'action' => $action,
+        $clientIP = "";
+        $this->auditLog->saveAuditLog([
+            'user_id'     => 'admin',
+            'action'      => $action,
             'description' => $description,
-            'client_ip' => $clientIP,
-            'module_name' => $module
-	    ]);
-    }    
+            'client_ip'   => $clientIP,
+            'module_name' => $module,
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * Product price attributes which needs to be updated via webservice.
+     *
+     * Here array keys represents the reference of attribute in the ARWebservice and
+     * array values represent product attributes.
+     *
+     * @return array
+     */
+    public function priceUpdateAttributes()
+    {
+        return [
+            'storeprice'        => 'price',
+            'special_price'     => 'special_price',
+            'cost'              => 'cost',
+            'ar_status'         => 'ar_status',
+            'special_from_date' => 'special_from_date',
+            'special_to_date'   => 'special_to_date',
+            'msrp'              => 'msrp',
+            'iqi'               => 'iqi',
+            'vendorrebate'      => 'vendor_rebate',
+            'customerrebate'    => 'customer_rebate',
+        ];
+    }
+
+    /**
+     * Prepare HTTP API request to send
+     *
+     * @param string $apiMethod
+     * @param string $methodType
+     * @return \Zend\Http\Request
+     */
+    protected function prepareRequest($apiMethod, $methodType)
+    {
+        if (!$this->request) {
+            $apiKey = $this->apiHelper->getApiKey();
+            $apiUrl = $this->apiHelper->getApiUrl();
+
+            $httpHeaders = new Headers();
+            $httpHeaders->addHeaders([
+                'Accept'       => 'application/json',
+                'Content-Type' => 'application/json',
+                'X-Api-Key'    => $apiKey,
+            ]);
+            $request = new Request();
+            $request->setHeaders($httpHeaders);
+            $this->request = $request;
+        }
+
+        $this->request->setUri($apiUrl . $apiMethod);
+        $this->request->setMethod($methodType);
+
+        return $this->request;
+    }
+
+    /**
+     * Prepare client
+     *
+     * @return \Zend\Http\Client
+     */
+    protected function prepareClient()
+    {
+        if (!$this->client) {
+            $client = new Client();
+            $options = [
+                'adapter'      => 'Zend\Http\Client\Adapter\Curl',
+                'curloptions'  => [CURLOPT_FOLLOWLOCATION => true],
+                'maxredirects' => 0,
+                'timeout'      => 360,
+            ];
+            $client->setOptions($options);
+            $this->client = $client;
+        }
+
+        return $this->client;
+    }
 }  
--- app/code/Dyode/PriceUpdate/Model/PriceUpdate.php
+++ app/code/Dyode/PriceUpdate/Model/PriceUpdate.php
@@ -4,212 +4,288 @@
  */
 namespace Dyode\PriceUpdate\Model;
 
-use \Magento\Framework\Model\AbstractModel;
-use Dyode\ARWebservice\Helper\Data;
+use Magento\Catalog\Model\Product;
+use Magento\Framework\DataObject;
+use Dyode\PriceUpdate\Helper\Data as PriceHelper;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
 
 /**
  * Price Model
+ *
  * @category Dyode
  * @package  Dyode_PriceUpdate
  * @module   PriceUpdate
  * @author   Nithin
  */
-class PriceUpdate extends \Magento\Framework\Model\AbstractModel
+class PriceUpdate
 {
 
-    public $productCollectionFactory;
-
-    public $skuList = array();
-
-    public $skus = array();
-
-    public $list = array();
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     */
+    protected $productCollectionFactory;
 
     /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
-     * @param \Dyode\PriceUpdate\Helper\Data $helper
-     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+     * @var \Magento\Catalog\Model\ResourceModel\Product\Collection
      */
-    public function __construct(
-        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        Data $apiHelper,
-        \Dyode\PriceUpdate\Helper\Data $priceHelper
-    ) {
+    protected $priceProductCollection;
+
+    /**
+     * @var \Dyode\PriceUpdate\Helper\Data
+     */
+    protected $helper;
+
+    /**
+     * @var []
+     */
+    protected $skuList;
+
+    /**
+     * @var []
+     */
+    protected $skuApiList;
+
+    /**
+     * PriceUpdate constructor.
+     *
+     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     * @param \Dyode\PriceUpdate\Helper\Data $priceHelper
+     */
+    public function __construct(ProductCollectionFactory $productCollectionFactory, PriceHelper $priceHelper)
+    {
         $this->productCollectionFactory = $productCollectionFactory;
-        $this->productRepository = $productRepository;
-        $this->apiHelper = $apiHelper;
         $this->helper = $priceHelper;
     }
 
     /**
-     * function name : updatePrice
-     * definition : cron model function for updatingprice
-     * @return no return
+     * Updating products price via cron
+     *
+     * @return bool
      */
     public function updatePrice()
     {
-       try {
-          $this->getProductSkulist();
-          $this->helper->setStock($this->list, 'dyode_priceupdate');
-          $this->getBatchPrice();
-          $this->processBatchprice();
-          //write admin logs
-          $this->helper->addLogs('price update', 'price updated successfully', 'dyode_priceupdate');
-        } catch (\Exception $exception) {
-          $this->helper->addLogs('price update', 'price update failed '.$exception, 'dyode_priceupdate');
-        }
-    }
-
-    /**
-     * function name : getProductSkulist
-     * definition : get sku and product id of all products
-     * @param int $storeViewId
-     * @return no return
-     */
-    public function getProductSkulist()
-    {
-        $productCollection = $this->productCollectionFactory->create();
-        /** Apply filters here */
-        $productCollection->addAttributeToSelect('*')
-        ->addAttributeToFilter('vendorId', 2139)
-        ->addAttributeToFilter('cron', 493);
-        $count = 0;
-        foreach ($productCollection as $product) {
-            $sku = utf8_encode(strtoupper(trim($product->getSku())));
-            $status = $product->getStatus();
-            $this->skuList[$sku]['entity_id'] = $product->getId();
-            $this->list[$count]['sku'] = $product->getSku();
-            if ($status) {
-              $this->list[$count]['active'] = true; 
-            } else {
-              $this->list[$count]['active'] = false;
-            }
-         $count++;   
-        }
-    }
-
-    /**
-     * function name : getBatchPrice
-     * definition : get price details of all products from AR
-     * @return no return
-     */
-    public function getBatchPrice()
-    {
-      $apiKey = $this->apiHelper->getApiKey();
-      $apiUrl = $this->apiHelper->getApiUrl();
         try {
-            $httpHeaders = new \Zend\Http\Headers();
-            $httpHeaders->addHeaders([
-               'Accept' => 'application/json',
-               'Content-Type' => 'application/json',
-               'X-Api-Key' => $apiKey
-            ]);
-            $request = new \Zend\Http\Request();
-            $request->setHeaders($httpHeaders);
-            $request->setUri($apiUrl.'getPrices?top50=false');
-            $request->setMethod(\Zend\Http\Request::METHOD_GET);
+            $skuList = $this->getProductSkuList();
+            $setStockResponse = $this->helper->sendSetStockARWebserviceRequest($skuList, 'dyode_priceupdate');
+            if (!$setStockResponse) {
+                return false;
+            }
 
-            $client = new \Zend\Http\Client();
-            $options = [
-               'adapter'   => 'Zend\Http\Client\Adapter\Curl',
-               'curloptions' => [CURLOPT_FOLLOWLOCATION => true],
-               'maxredirects' => 0,
-               'timeout' => 30
-            ];
-            $client->setOptions($options);
-            $this->helper->addLogs('getPrice API calling', $request, 'dyode_priceupdate');
-            $response = $client->send($request);
-            $data = json_decode($response->getBody());
-            $list = $data->LIST;
-            foreach ($list as $key) {
-                $obj = array(
-                  'storeprice' => $key->price->STOREPRICE,
-                  'special_price' => $key->price->SPECIALPRICE,
-                  'vendorrebate' => $key->price->VENDORREBATE,
-                  'customerrebate' => $key->price->CUSTOMERREBATE,
-                  'recycling_price' => $key->price->RECYCLEAMOUNT,
-                  'recycling_description' => $key->price->RECYCLEINFO,
-                  'cost' => $key->price->COST,
-                  'msrp' => $key->price->MSRP,
-                  'sku' => utf8_encode(trim($key->item_id)),
-                  'iqi' => trim( $key->price->IQI_STATUS ),
-                  'ar_status' => trim( $key->price->ITEM_TYPE1 )
-                );
-                $this->skus[ $obj['sku'] ]= $obj;
+            $updatePriceResponse = $this->helper->sendUpdatePriceARWebserviceRequest('dyode_priceupdate');
+            if (!$updatePriceResponse) {
+                return false;
             }
-            if ($data->CONTINUE) {
-               $this->getBatchPrice();
-            }
-          $this->helper->addLogs('getPrice API calling', $response->getBody(), 'dyode_priceupdate');
-        }
-        catch (\Exception $e) {
-          $this->helper->addLogs('getPrice API calling', 'connection failed'.$e, 'dyode_priceupdate');return false;
+
+            $this->updateProductPriceInfo($updatePriceResponse);
+            $this->helper->addLogs('price update', 'price updated successfully', 'dyode_priceupdate');
+
+            return true;
+        } catch (\Exception $exception) {
+            $this->helper->addLogs('price update', 'price update failed ' . $exception, 'dyode_priceupdate');
+
+            return false;
         }
     }
 
     /**
-     * function name : processBatchprice
-     * definition : update price details of all products
-     * @return no return
+     * get sku and product id of all products
+     *
+     * @param boolean $api
+     * @return array $skuList
      */
-    public function processBatchprice()
+    public function getProductSkuList($api = true)
+    {
+        if (!$this->skuList || !$this->skuApiList) {
+            $count = 0;
+            $skuList = [];
+            $skuList4Api = [];
+
+            foreach ($this->productCollectionForPriceUpdate() as $product) {
+                $sku = utf8_encode(strtoupper(trim($product->getSku())));
+                $status = $product->getStatus();
+                $skuList[$sku]['entity_id'] = $product->getId();
+                $skuList4Api[$count]['sku'] = $product->getSku();
+                if ($status) {
+                    $skuList4Api[$count]['active'] = true;
+                } else {
+                    $skuList4Api[$count]['active'] = false;
+                }
+                $count++;
+            }
+
+            $this->skuList = $skuList;
+            $this->skuApiList = $skuList4Api;
+        }
+
+        if ($api) {
+            return $this->skuApiList;
+        }
+
+        return $this->skuList;
+    }
+
+    /**
+     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
+     */
+    public function productCollectionForPriceUpdate()
+    {
+        if (!$this->priceProductCollection) {
+            /** @var \Magento\Catalog\Model\ResourceModel\Product\Collection $productCollection */
+            $productCollection = $this->productCollectionFactory->create();
+            $productCollection->addAttributeToSelect(['sku', 'status', 'vendorid', 'cron'])
+                ->addAttributeToSelect(array_values($this->helper->priceUpdateAttributes()))
+                ->addAttributeToFilter('vendorid', 2139)
+                ->addAttributeToFilter('cron', 493)
+                ->setPageSize(3)
+                ->getCurPage(1);
+
+            $this->priceProductCollection = $productCollection;
+        }
+
+        return $this->priceProductCollection;
+    }
+
+    /**
+     * Update price details of all products
+     *
+     * @param array $priceInfo
+     */
+    public function updateProductPriceInfo(array $priceInfo)
     {
         $special_to_date = date('Y-m-d', strtotime("+2 days"));
         $special_from_date = date('Y-m-d', time());
-        foreach ($this->skus as $item) {
+        $store = 0;
+
+        foreach ($priceInfo as $item) {
             $sku = utf8_encode(strtoupper(trim($item['sku'])));
-            if (array_key_exists($sku, $this->skuList)) {
-            $entity_id = $this->skuList[$sku]['entity_id'];
-                if (!empty($entity_id)) {
-                    $price = $item['storeprice'];
-                    $special_price = $item['special_price'];
-                    $vendor_rebate = $item['vendorrebate'];
-                    $customer_rebate = $item['customerrebate'];
-                    $recycling_price = $item['recycling_price'];
-                    $recycling_description = $item['recycling_description'];
-                    $cost = $item['cost'];
-                    $msrp = $item['msrp'];
-                    $sku = $item['sku'];
-                    $iqi = $item['iqi'];
-                    $ar_status = $item['ar_status'];
-                    $cost = $cost - $vendor_rebate;
-                    
-                    if(!isset($customer_rebate)){
-                        $customer_rebate = 0;
-                        $special_price = '';
+
+            if (!array_key_exists($sku, $this->getProductSkuList(false))) {
+                continue;
+            }
+
+            $productId = $this->skuList[$sku]['entity_id'];
+            if (!empty($productId)) {
+                /** @var \Magento\Catalog\Model\Product $product */
+                $itemPriceInfo = new DataObject($item);
+                $product = $this->productCollectionForPriceUpdate()->getItemById($productId);
+                $updateAttributes = [];
+                $exclude = ['cost', 'special_price', 'special_from_date', 'special_to_date'];
+
+                //collect attributes whose values really changed from the current product attribute value.
+                foreach ($this->helper->priceUpdateAttributes() as $apiKey => $productKey) {
+                    if ($itemPriceInfo->getData($apiKey)
+                        && !in_array($productKey, $exclude)
+                        && $itemPriceInfo->getData($apiKey) != $product->getData($productKey)
+                    ) {
+                        $updateAttributes[] = $apiKey;
                     }
-                    $rebated_price = $price - $customer_rebate;
-                    if (($special_price == 0) || ($rebated_price < $special_price) && $customer_rebate != 0){
-                        $special_price = $rebated_price;
-                    }
-                    if (0 < $special_price && $special_price < $price) {
-                        $specialprice = $special_price;
-                        // $specialfromdate = $special_from_date;
-                        // $specialtodate = $special_to_date;
+                }
+
+                //checks the 'cost' attribute value is changed.
+                if (!$this->isCostSame($itemPriceInfo, $product)) {
+                    $updateAttributes[] = 'cost';
+                }
+
+                //checks the 'special_price' attribute value is changed.
+                if (!$this->isSpecialPriceSame($itemPriceInfo, $product)) {
+                    $updateAttributes[] = 'special_price';
+                    $updateAttributes[] = 'special_from_date';
+                    $updateAttributes[] = 'special_to_date';
+                }
+
+                //Update only changed attribute values.
+                foreach ($updateAttributes as $attribute) {
+                    if ($attribute === 'cost') {
+                        $product->addAttributeUpdate('cost', $this->calculateProductCost($itemPriceInfo), $store);
+                    } elseif ($attribute === 'special_price') {
+                        $product->addAttributeUpdate('special_price', $this->calculateSpecialPrice($itemPriceInfo),
+                            0);
+                    } elseif ($attribute === 'special_from_date') {
+                        $product->addAttributeUpdate('special_from_date', $special_from_date, $store);
+                    } elseif ($attribute === 'special_to_date') {
+                        $product->addAttributeUpdate('special_to_date', $special_to_date, $store);
                     } else {
-                        $specialprice = '';
-                        // $specialfromdate = '';
-                        // $specialtodate = '';
+                        $product->addAttributeUpdate($attribute, $itemPriceInfo->getData($attribute), $store);
                     }
-                    $product = $this->productRepository->getById($entity_id);
-                    $product->setStoreId(0);
-                    $product->setPrice($price);
-                    $product->setSpecialPrice($specialprice);
-                    $product->setCost($cost);
-                    $product->setArStatus($ar_status);
-                    //$product->setSpecialFromDate($specialfromdate);
-                    //$product->setSpecialToDate($specialtodate);
-                    $product->setMsrp($msrp);
-                    $product->setIqi($iqi);
-                    $product->setRecyclingprice($recycling_price);
-                    $product->setRecyclingdescription($recycling_description);
-                    $product->setVendorRebate($vendor_rebate);
-                    $product->setCustomerRebate($customer_rebate);
-                    $product->save();
                 }
             }
         }
+    }
+
+    /**
+     * Calculating cost Price of the product from the api response.
+     *
+     * @param \Magento\Framework\DataObject $priceInfo
+     * @return float
+     */
+    protected function calculateProductCost(DataObject $priceInfo)
+    {
+        $cost = (float)$priceInfo->getCost();
+        if ($priceInfo->getCost() && $priceInfo->getVendorrebate()) {
+            $cost = (float)($priceInfo->getCost() - $priceInfo->getVendorrebate());
+        }
+
+        return $cost;
+    }
+
+    /**
+     * Calculate Special price from the api response
+     *
+     * @param \Magento\Framework\DataObject $priceInfo
+     * @return float
+     */
+    protected function calculateSpecialPrice(DataObject $priceInfo)
+    {
+        $price = (float)$priceInfo->getStoreprice();
+        $special_price = (float)$priceInfo->getSpecialPrice();
+        $customer_rebate = (float)$priceInfo->getCustomerrebate();
+        $rebated_price = $price - $customer_rebate;
+        if (($special_price == 0) || ($rebated_price < $special_price)) {
+            $special_price = $rebated_price;
+        }
+
+        return $special_price;
+    }
+
+    /**
+     * Checks whether the product 'cost' and priceInfo 'cost' are same.
+     *
+     * @param \Magento\Framework\DataObject $priceInfo
+     * @param \Magento\Catalog\Model\Product $product
+     * @return bool
+     */
+    protected function isCostSame(DataObject $priceInfo, Product $product)
+    {
+        $cost = (float)$priceInfo->getCost();
+        if ($priceInfo->getCost() && $priceInfo->getVendorrebate()) {
+            $cost = (float)($priceInfo->getCost() - $priceInfo->getVendorrebate());
+        }
+
+        return $cost == (float)$product->getPrice();
+    }
+
+    /**
+     * Checks whether the product 'special_price' and PriceInfo 'special_price' are same.
+     *
+     * @param \Magento\Framework\DataObject $priceInfo
+     * @param \Magento\Catalog\Model\Product $product
+     * @return bool
+     */
+    protected function isSpecialPriceSame(DataObject $priceInfo, Product $product)
+    {
+        $price = (float)$priceInfo->getStoreprice();
+        $special_price = (float)$priceInfo->getSpecialPrice();
+        $customer_rebate = (float)$priceInfo->getCustomerrebate();
+        $rebated_price = $price - $customer_rebate;
+        if (($special_price == 0) || ($rebated_price < $special_price)) {
+            $special_price = $rebated_price;
+        }
+        if (0 < $special_price && $special_price < $price) {
+            if ($special_price != (float)$product->getSpecialPrice()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
